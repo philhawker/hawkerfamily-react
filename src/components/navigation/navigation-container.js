1f4147da484fbd610d9068ed0c03534fce668263
Phil Hawker <philhawker1211@gmail.com>
2020-05-20 17:26:27 -0600

    moved all header and logo code to its own component. however, original portfolio navbar is now visible

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -28,11 +28,42 @@ const NavigationComponent = (props) => {
     }
 
     return (
-        <div>
-            {props.loggedInStatus === 'LOGGED_IN' ? dynamicLink('/portfolio-manager', 'Portfolio Manager') : null}
+        <div className='nav-wrapper'>
+            <div className='left-side'>
+                <div className='nav-link-wrapper'>
+                    <NavLink exact to='/' activeClassName='nav-link-active'>
+                        Home
+                    </NavLink>
+                </div>
+
+                <div className='nav-link-wrapper'>
+                    <NavLink to='/about-me' activeClassName='nav-link-active'>
+                        About
+                    </NavLink>
+                </div>
+
+                <div className='nav-link-wrapper'>
+                    <NavLink to='/contact' activeClassName='nav-link-active'>
+                        Contact
+                    </NavLink>
+                </div>
+
+                <div className='nav-link-wrapper'>
+                    <NavLink to='/blog' activeClassName='nav-link-active'>
+                        Blog
+                    </NavLink>
+                </div>
+
+                {props.loggedInStatus === 'LOGGED_IN' ? dynamicLink('/portfolio-manager', 'Portfolio Manager') : null}
+            </div>
+
+            <div className='right-side'>
+                PHIL M. HAWKER
+
             {props.loggedInStatus === 'LOGGED_IN' ? <a onClick={handleSignOut}>
-                <FontAwesomeIcon icon='sign-out-alt' />
-            </a> : null}
+                    <FontAwesomeIcon icon='sign-out-alt' />
+                </a> : null}
+            </div>
         </div>
     )
 }
--- src/components/pages/sassafras.js
+++ src/components/pages/sassafras.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import flowerbg from "../../../static/assets/images/flowerbg.jpg"
+import Header from "../header"
 
 export default class Sassafras extends Component {
   constructor(props) {
@@ -9,72 +9,8 @@ export default class Sassafras extends Component {
 
   render() {
     return (
-      <div className="content-page-wrapper background home blog custom-header header-image header-full-width content-sidebar genesis-breadcrumbs-hidden genesis-footer-widgets-visible front-page">
-        <a href="#0" class="to-top" title="Back To Top">Top</a>
-        <div className="site-container">
-          <div className="wrap1">
-            <div className="wrap2">
-              <header class="site-header" itemscope="" itemtype="https://schema.org/WPHeader">
-                <div class="wrap">
-                  <nav class="nav-secondary" aria-label="Secondary" itemscope=""
-                    itemtype="https://schema.org/SiteNavigationElement">
-                    <div class="wrap">
-                      <div class="responsive-menu-icon"></div>
-                      <ul id="menu-secondary-menu" class="menu genesis-nav-menu menu-secondary responsive-menu">
-
-
-
-                        <li class="search"><a id="main-nav-search-link" class="icon-search"></a>
-                          <div class="search-div">
-                            <form class="search-form" method="get" action="https://hawkerfamily.com/" role="search"
-                              itemprop="potentialAction" itemscope="" itemtype="https://schema.org/SearchAction">
-                              <input
-                                class="search-form-input" type="search" name="s" id="searchform-1"
-                                placeholder="Search this website" itemprop="query-input" />
-                              <input class="search-form-submit"
-                                type="submit" value="Search" />
-                            </form>
-                          </div>
-                        </li>
-                        <aside class="widget-area"></aside>
-                      </ul>
-                    </div>
-                  </nav>
-                  <div class="title-area">
-                    <p class="site-title" itemprop="headline"><a href="/">Hawker Family</a></p>
-                  </div>
-                </div>
-              </header>
-
-              {/* NAVBAR START */}
-              <nav class="nav-primary" aria-label="Main" itemscope="" itemtype="https://schema.org/SiteNavigationElement">
-                <div class="wrap">
-                  <div class="responsive-menu-icon">
-                  </div>
-                  <ul id="menu-primary-menu-2" class="menu genesis-nav-menu menu-primary responsive-menu">
-                    <li id="menu-item-735" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-735">
-                      <a href="https://hawkerfamily.com" aria-current="page" itemprop="url">
-                        <span itemprop="name">Home</span>
-                      </a>
-                    </li>
-                    <li id="menu-item-736" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-736">
-                      <a href="https://hawkerfamily.com/aboutus" itemprop="url">
-                        <span itemprop="name">About Us</span>
-                      </a>
-                    </li>
-                    <li id="menu-item-737" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-737">
-                      <a href="https://hawkerfamily.com/our" itemprop="url">
-                        <span itemprop="name">Our Faith</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-
-              {/* NAVBAR END */}
-            </div>
-          </div>
-        </div>
+      <div>
+        <Header />
       </div>
     )
   }
