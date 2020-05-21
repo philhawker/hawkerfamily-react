34aefcc86607e33dbf4a53abe7847d72bb2209b9
Phil Hawker <philhawker1211@gmail.com>
2020-05-20 18:45:30 -0600

    removed scroll bars and added min-max device width breakpoints

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,17 +5,32 @@ import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/app";
 import reducers from "./reducers";
+import ReactBreakpoints from "react-breakpoints"
+import { RemoveScrollBar } from 'react-remove-scroll-bar'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const breakpoints = {
+    mobile: 320,
+    mobileLandscape: 480,
+    tablet: 768,
+    tabletLandscape: 1024,
+    desktop: 1200,
+    desktopLarge: 1500,
+    desktopWide: 1920,
+}
 
 import "./style/main.scss";
 
 function main() {
     ReactDOM.render(
-        <Provider store={ createStoreWithMiddleware(reducers) }>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+        <Provider store={createStoreWithMiddleware(reducers)}>
+            <ReactBreakpoints breakpoints={breakpoints}>
+                <RemoveScrollBar />
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ReactBreakpoints>
         </Provider>,
         document.querySelector(".app-wrapper")
     );
